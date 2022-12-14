1f68433b2eeb328bc8ec7ec323bbeae68e1dc92c
EC2 Default User <ec2-user@ip-172-31-18-113.ap-northeast-1.compute.internal>
2022-12-14 21:43:41 +0000

    [Add] after_sign_in_path_for

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ApplicationRecord
 
-  has_one_attached :image
+  has_one_attached :profile_image
   belongs_to :user
 
-  def get_image(width, height)
+  def get_profile_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image0.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
