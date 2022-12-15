80b1f4133631bcfa5aed887c70535f5de4f9ae3f
EC2 Default User <ec2-user@ip-172-31-18-113.ap-northeast-1.compute.internal>
2022-12-15 11:59:48 +0000

    [Update] 部分テンプレート

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,11 +4,11 @@ class Book < ApplicationRecord
   belongs_to :user
 
   def get_profile_image(width, height)
-    unless image.attached?
+    unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image0.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
     end
-    image.variant(resize_to_limit: [width, height]).processed
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
