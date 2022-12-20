56f858177d59258cd6411687c49733e93449a085
EC2 Default User <ec2-user@ip-172-31-18-113.ap-northeast-1.compute.internal>
2022-12-20 11:31:53 +0000

    [Add] Rspecテストの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @book = Book.new
@@ -49,6 +50,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(book_path) unless @user == current_user
   end
 
 end
